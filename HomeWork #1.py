1ccaa726bf84598baba99614e5557733e62242e0
Andrew <anryzhakov@yandex.ru>
2024-10-27 17:22:25 +0300

    Add files via upload

--- HomeWork #1.py
+++ HomeWork #1.py
@@ -8,18 +8,13 @@ import random
 def add_everything_up(a, b):
     try:
         s = a + b
-        return s
     except TypeError:
         print('Переменные разных типов функция сложения не поддерживает')
         print('Программа вынуждена привести переменные к типу string')
         a1 = str(a)
         b1 = str(b)
         s = a1 + b1
-        return s
-    else:
-        print('Тип, используемых переменных позволяет провести сложение без ошибки')
-    finally:
-        pass
+    return s
 
 
 items = [1, 7, 22, 3.95, 7.87, 4.03, 'apple', 'string', 'float', 'grep']
